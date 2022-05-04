eedfa047abc23feb95d41211e66ee52c4f4568bf
Eli Craig <elidcraig@gmail.com>
2022-05-04 08:37:54 -0700

    images appended to DOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-fetch('http://localhost:3000')
-.then(resp => resp.json)
+fetch('http://localhost:3000/ramens')
+.then(resp => resp.json())
 .then(ramens => {
-    
+
+    const menuElement = document.querySelector('div#ramen-menu')
+
+    ramens.forEach(ramenObj => {
+        const newImgElement = document.createElement('img')
+        newImgElement.src = ramenObj.image
+        menuElement.appendChild(newImgElement)
+    })
 })
